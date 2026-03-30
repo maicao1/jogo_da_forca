43b626b364576a4ba82e958a51eeb853e25d567e
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-03-30 11:48:58 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,25 +3,41 @@ def jogar_forca():
     print("\nBem vindo ao jogo da forca!\n")
     print("-------------------------------")
 
-    lista = ""
-
-
-    palavra_secreta = "processador"
+    palavra_secreta = "processador".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     perdeu = False
     acertou = False
+    erros = 0
 
-    while not perdeu and not acertou:
+    arquivo = open ("jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in palavras:
+        linha = linha.strip()
+        palavras.append(linha)
+        
+
+    while (not perdeu and not acertou):
         chute = input("Escreva uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         #index define a posição da letra
         index = 0
+        if (chute in palavra_secreta ):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+         
+        else: 
+         erros = erros + 1
+        
+         perdeu = erros == 6
 
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                print(f"A letra {chute} está na posição {index}")
-            index = index + 1
+        acertou = "_" not in letras_acertadas
+         
 
+        print(letras_acertadas)
 if(__name__ == "__main__"):
     jogar_forca()
 
